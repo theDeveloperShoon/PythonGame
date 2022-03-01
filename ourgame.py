00fa16ea212896ef0bab1ff4b2e74cc789dfd393
Sean Cotter <seancotter04@hotmail.com>
2022-03-01 17:15:45 -0500

    Add pygame

---
+++ ourgame.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+
+
+class Game:
+    def __init__(self):
+        self.screen_width = 640
+        self.screen_height = 640
+        self.resolution = self.screen_width, self.screen_height
+
+    def inititalize(self):
+        pygame.init()
+        pygame.display.set_mode(self.resolution)
+
+
+game = Game()
+game.inititalize()
+
+# Game Loop
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
