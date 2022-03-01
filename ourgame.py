d6c02ff762afef3936008da50b38d39130e7c689
Sean Cotter <seancotter04@hotmail.com>
2022-03-01 17:55:16 -0500

    Changed the loop's 1 into True

--- ourgame.py
+++ ourgame.py
@@ -17,7 +17,8 @@ game = Game()
 game.inititalize()
 
 # Game Loop
-while 1:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
