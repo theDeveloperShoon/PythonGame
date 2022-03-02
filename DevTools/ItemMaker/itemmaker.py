69675246506684190470583f7cfe21869585f40c
Sean Cotter <seancotter04@hotmail.com>
2022-03-01 19:29:37 -0500

    Makes ItemMaker into a class.

--- DevTools/ItemMaker/itemmaker.py
+++ DevTools/ItemMaker/itemmaker.py
@@ -1,9 +1,15 @@
 import json
+import os
 from tkinter import *
 from tkinter import ttk
 
 
-# Classes
+# Checks if the json data exists
+def jsonDataExists():
+    return os.path.isfile("Data/itemlist.json")
+
+
+# Defining ItemList class
 class ItemList:
     def __init__(self):
         self.items = []
@@ -12,11 +18,26 @@ class ItemList:
         return json.dumps(self, default=lambda x: x.__dict__, indent=4)
 
 
+# Defining Item Class
 class Item:
     def __init__(self):
         self.name = "TestSword"
         self.description = "A test sword"
         self.durability = 100
+
+
+# Defining ItemMaker Class
+class ItemMaker():
+
+    # Defining Constructor
+    def __init__(self, root):
+        self.root = root
+        self.root.title("ItemMaker")
+        self.root.geometry('600x600')
+
+        if not jsonDataExists():
+            self.itemList = ItemList()
+            print("Json file doesn't exist")
 
 
 # Side Code
@@ -26,7 +47,11 @@ items.items.append(item1)
 items.items.append(item1)
 print(items.toJson())
 
+# Inititalizes Tkinter
 root = Tk()
-root.title("ItemMaker")
-root.geometry('600x600')
+
+# Initialize ItemMaker
+ItemMaker(root)
+
+# Runs the TKinter Loop
 root.mainloop()
