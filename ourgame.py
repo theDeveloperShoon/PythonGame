eeaad12b76e5d6d69359a64dfeb8c34f53458fd1
Sean Cotter <seancotter04@hotmail.com>
2022-03-04 22:47:17 -0500

    Renders a box onto the screen

--- ourgame.py
+++ ourgame.py
@@ -10,12 +10,18 @@ class GameEngine:
 
     def inititalize(self):
         pygame.init()
-        pygame.display.set_mode(self.resolution)
+        self.screen = pygame.display.set_mode(self.resolution)
         pygame.display.set_caption("TemporaryGameName")
 
 
 game = GameEngine()
 game.inititalize()
+
+speed = [1, 2]
+black = 0, 0, 0
+
+box = pygame.image.load("Assets/Sprites/TestBox.png")
+boxrect = box.get_rect(center=(32, 32))
 
 # Game Loop
 while True:
@@ -23,3 +29,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    game.screen.fill(black)  # Fills the background with black
+    game.screen.blit(box, boxrect)  # Adds the box onto screen
+    pygame.display.flip()  # Updates the full display Surface to the screen
