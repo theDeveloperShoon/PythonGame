67e9706a0ac1b8c13fdf3da2e9a330e3f14c2d8b
Sean Cotter <seancotter04@hotmail.com>
2022-03-10 16:14:02 -0500

    Added a rough version of movement

--- ourgame.py
+++ ourgame.py
@@ -1,28 +1,36 @@
 import sys
 import pygame
+import pygame.locals
 
 
 class GameEngine:
     def __init__(self):
+        pygame.init()
         self.screen_width = 640
         self.screen_height = 640
         self.resolution = self.screen_width, self.screen_height
-
-    def inititalize(self):
-        pygame.init()
         self.screen = pygame.display.set_mode(self.resolution)
         pygame.display.set_caption("TemporaryGameName")
+
+
+class GameObject:
+    def __init__(self, sprite, speed):
+        self.speed = speed
+        self.sprite = sprite
 
 
 class Player:
     def __init__(self, pathToSprite):
         self.sprite = pygame.image.load(pathToSprite)
-        self.spriterect = self.sprite.get_rect()
         self.speed = [0, 0]
+        self.xLoc = 32
+        self.yLoc = 32
+
+    def get_coordinate(self):
+        return (self.xLoc, self.yLoc)
 
 
 game = GameEngine()
-game.inititalize()
 
 black = 0, 0, 0
 
@@ -35,9 +43,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                testPlayer.xLoc -= 4
+            if event.key == pygame.K_d:
+                testPlayer.xLoc += 4
+
     game.screen.fill(black)  # Fills the background with black
 
     # Adds the box onto screen
-    game.screen.blit(testPlayer.sprite, testPlayer.spriterect)
+    game.screen.blit(testPlayer.sprite, testPlayer.get_coordinate())
 
     pygame.display.flip()  # Updates the full display Surface to the screen
