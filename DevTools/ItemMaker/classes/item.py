a594475da20b08f1b6637a61cd3a41198fc37b11
Sean Cotter <seancotter04@hotmail.com>
2022-03-23 16:19:55 -0400

    Removed item and itemlist from itemmaker.py and made their own file

---
+++ DevTools/ItemMaker/classes/item.py
@@ -0,0 +1,8 @@
+# Defining Item Class
+class Item:
+    # Defining Constructor
+    def __init__(self):
+        # Defines class variables
+        self.name = "NewItem"
+        self.description = "A new item"
+        self.durability = 100
--- DevTools/ItemMaker/itemmaker.py
+++ DevTools/ItemMaker/itemmaker.py
@@ -3,38 +3,8 @@ import os
 import io
 from tkinter import *
 from tkinter import filedialog
-
-
-# Defining ItemList class
-class ItemList:
-    # Defining Constructor
-    def __init__(self):
-        self.items = []
-
-    # Makes it 'nicer' to append items
-    def append(self, item):
-        self.items.append(item)
-
-    # Returns the items in the list
-    def getItems(self):
-        return self.items
-
-    def removeItem(self, index):
-        self.items.pop(index)
-
-    # Converts the item list to json
-    def toJson(self):
-        return json.dumps(self, default=lambda x: x.__dict__, indent=4)
-
-
-# Defining Item Class
-class Item:
-    # Defining Constructor
-    def __init__(self):
-        # Defines class variables
-        self.name = "NewItem"
-        self.description = "A new item"
-        self.durability = 100
+from classes.item import Item
+from classes.itemlist import ItemList
 
 
 # Defining ItemMaker Class
