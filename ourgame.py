886d7f1bb4c4b20dc065762a897e507dc49b806f
Sean Cotter <seancotter04@hotmail.com>
2022-03-26 21:52:56 -0400

    Made Player a child of Entity, so we can add stats that other entities may not have

--- ourgame.py
+++ ourgame.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 import pygame.locals
+from Game.Entity import Entity
 
 
 # Classes
@@ -21,30 +22,14 @@ class GameObject:
         self.sprite = sprite
 
 
-class Player:
-    def __init__(self, pathToSprite):
-        self.sprite = pygame.image.load(pathToSprite)
-        self.speed = [0, 0]
-        self.x_velocity = 0
-        self.y_velocity = 0
-        self.xLoc = 64
-        self.yLoc = 64
-        self.x_offset = 0
-        self.y_offset = 0
+class Player(Entity):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.health = 64
 
-    def set_offset(self, horizontal_offset, vertical_offset):
-        self.x_offset = horizontal_offset
-        self.y_offset = vertical_offset
-
-    def update_location(self):
-        self.xLoc += self.x_velocity
-        self.yLoc += self.y_velocity
-
-    def get_coordinate(self):
-        return (self.xLoc + self.x_offset, self.yLoc + self.y_offset)
+    # Functions
 
 
-# Functions
 def HorizontalMovementHandler(keys):
     returnModifier = 0
 
