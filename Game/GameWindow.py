3cafde24e3b9083233f7508957d81957a84e8eeb
Sean Cotter <seancotter04@hotmail.com>
2022-03-27 13:48:46 -0400

    Created framework for making a GameWindow class

---
+++ Game/GameWindow.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class GameWindow():
+    def __init__(self, width=640, height=640):
+        self.window_width = width
+        self.window_height = height
+
+    def create_window():
+        test = 1
