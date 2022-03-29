2888f62b21cb894fd644d5aca0658df818c6b7cd
Sean Cotter <seancotter04@hotmail.com>
2022-03-28 22:43:24 -0400

    Finished the first version of the GameWindow.

--- Game/GameWindow.py
+++ Game/GameWindow.py
@@ -5,6 +5,8 @@ class GameWindow():
     def __init__(self, width=640, height=640):
         self.window_width = width
         self.window_height = height
+        self.resolution = self.window_width, self.window_height
 
-    def create_window():
-        test = 1
+    def create_window(self):
+        screen = pygame.display.set_mode(self.resolution)
+        return screen
